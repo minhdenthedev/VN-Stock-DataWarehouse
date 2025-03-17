a4c4179e85dfee203b07e85fd450e8b83f35b677
minhdenthedev <minhdenthedev@github.com>
2025-03-17 15:54:41 +0700

    update pipeline and data

---
+++ scripts/database_utils.py
@@ -0,0 +1,200 @@
+import pandas as pd
+import psycopg2
+
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "vn_stock_dw",
+    "user": "postgres",
+    "password": "12",
+    "port": 5432,
+}
+
+
+def check_database_exists(dbname, user, password, host="localhost", port="5432"):
+    try:
+        # Connect to the default "postgres" database
+        conn = psycopg2.connect(dbname="postgres", user=user, password=password, host=host, port=port)
+        conn.autocommit = True  # Allow running DB-level queries
+        cur = conn.cursor()
+
+        # Check if database exists
+        cur.execute(f"SELECT 1 FROM pg_database WHERE datname = %s;", (dbname,))
+        exists = cur.fetchone() is not None
+
+        # Close connection
+        cur.close()
+        conn.close()
+
+        return exists
+
+    except psycopg2.Error as e:
+        print(f"Error: {e}")
+        return False
+
+
+def branching_postgres():
+    if check_database_exists("vn_stock_dw", "postgres", "12"):
+        return "skip_create_db"
+    return "initiate_postgres"
+
+
+def insert_industries():
+    csv_path = "/home/m1nhd3n/Works/DataEngineer/DataFlow2025Task2/data/raw/industries.csv"
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+    try:
+        # Read CSV file
+        df = pd.read_csv(csv_path, sep="|", index_col=False)
+
+        # Convert dataframe to list of tuples
+        data_tuples = [tuple(x) for x in df.to_numpy()]
+
+        # Create INSERT query dynamically
+        columns = ', '.join(df.columns)
+        values_placeholder = ', '.join(['%s'] * len(df.columns))
+        insert_query = f"""
+        INSERT INTO dim_enterprises_industries ({columns}) 
+        VALUES ({values_placeholder})
+        ON CONFLICT (industry_code) 
+        DO UPDATE SET 
+            {', '.join([f"{col} = EXCLUDED.{col}" for col in df.columns if col != "stock_code"])}
+        """
+
+        # Execute batch insert
+        cur.executemany(insert_query, data_tuples)
+        conn.commit()
+
+        print(f"Successfully inserted {len(data_tuples)} rows into dim_enterprises_industries")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+
+def insert_dates(dates):
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+    try:
+        # Convert dataframe to list of tuples
+        df = pd.DataFrame({"date_key": dates})
+        df["date_key"] = pd.to_datetime(df["date_key"], format="%d/%m/%Y")
+
+        # Calculate date attributes
+        df["year"] = df["date_key"].dt.year
+        df["quarter"] = df["date_key"].dt.quarter
+        df["month"] = df["date_key"].dt.month
+        df["week"] = df["date_key"].dt.isocalendar().week
+        df["day_of_week"] = df["date_key"].dt.dayofweek + 1  # 1=Monday, 7=Sunday
+
+        data_tuples = [tuple(x) for x in df.to_numpy()]
+
+        # Create INSERT query dynamically
+        columns = ', '.join(df.columns)
+        values_placeholder = ', '.join(['%s'] * len(df.columns))
+        insert_query = f"""
+            INSERT INTO dim_date ({columns}) 
+            VALUES ({values_placeholder})
+            ON CONFLICT (date_key) 
+            DO UPDATE SET 
+                {', '.join([f"{col} = EXCLUDED.{col}" for col in df.columns if col != "date_key"])}
+            """
+
+        # Execute batch insert
+        cur.executemany(insert_query, data_tuples)
+        conn.commit()
+
+        print(f"Successfully inserted {len(data_tuples)} rows into dim_date")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+
+def insert_enterprise():
+    csv_path = "/home/m1nhd3n/Works/DataEngineer/DataFlow2025Task2/data/raw/enterprises.csv"
+    # Read CSV file
+    df = pd.read_csv(csv_path, sep="|", index_col=False)
+    df = df.drop("cafef_url", axis="columns")
+    df = df.rename({"code": "stock_code", "name": "enterprise_name"}, axis="columns")
+    dates = df['listing_date'].tolist()
+    insert_dates(dates)
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+    try:
+        # Convert dataframe to list of tuples
+        df["listing_date"] = pd.to_datetime(df["listing_date"], format="%d/%m/%Y")
+        data_tuples = [tuple(x) for x in df.to_numpy()]
+
+        # Create INSERT query dynamically
+        columns = ', '.join(df.columns)
+        values_placeholder = ', '.join(['%s'] * len(df.columns))
+        insert_query = f"""
+        INSERT INTO dim_enterprises ({columns}) 
+        VALUES ({values_placeholder})
+        ON CONFLICT (stock_code) 
+        DO UPDATE SET 
+            {', '.join([f"{col} = EXCLUDED.{col}" for col in df.columns if col != "stock_code"])}
+        """
+
+        # Execute batch insert
+        cur.executemany(insert_query, data_tuples)
+        conn.commit()
+
+        print(f"Successfully inserted {len(data_tuples)} rows into dim_enterprises")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        print(df["listing_date"])
+    finally:
+        cur.close()
+        conn.close()
+
+
+def insert_sub_companies():
+    csv_path = "/home/m1nhd3n/Works/DataEngineer/DataFlow2025Task2/data/raw/sub_companies.csv"
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+    try:
+        # Read CSV file
+        df = pd.read_csv(csv_path, sep="|", index_col=False)
+        df = df.rename({
+            "stock_code": "parent_stock_code",
+            "comp_name": "sub_company_name",
+            "comp_type": "company_type"
+        }, axis="columns")
+        df["ownership_ratio"] = df["ownership_ratio"].apply(lambda x: str(x).replace("%", ""))
+        df["charter_capital"] = df["charter_capital"].apply(lambda x: str(x).replace(",", ""))
+        df["contributed_capital"] = df["contributed_capital"].apply(lambda x: str(x).replace(",", ""))
+
+        # Convert dataframe to list of tuples
+        data_tuples = [tuple(x) for x in df.to_numpy()]
+
+        # Create INSERT query dynamically
+        columns = ', '.join(df.columns)
+        values_placeholder = ', '.join(['%s'] * len(df.columns))
+        insert_query = f"""
+            INSERT INTO dim_enterprises_sub_companies ({columns}) 
+            VALUES ({values_placeholder})
+            ON CONFLICT (parent_stock_code, sub_company_name) 
+            DO UPDATE SET 
+                {', '.join([f"{col} = EXCLUDED.{col}" for col in df.columns if col not in ["parent_stock_code",
+                                                                                           "sub_company_name"]])}
+            """
+
+        # Execute batch insert
+        cur.executemany(insert_query, data_tuples)
+        conn.commit()
+
+        print(f"Successfully inserted {len(data_tuples)} rows into dim_enterprises_sub_companies")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        cur.close()
+        conn.close()
--- scripts/etl_pipeline.py
+++ scripts/etl_pipeline.py
@@ -1,15 +1,17 @@
 import pandas as pd
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 from get_enterprise_utils import *
+from database_utils import *
 import os
 
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 
 NIEM_YET_URL = "https://cafef.vn/du-lieu/du-lieu-doanh-nghiep.chn"
-QUY_URL = "https://cafef.vn/du-lieu/danh-sach-cac-quy-dau-tu.chn"
-NGAN_HANG_URL = "https://cafef.vn/du-lieu/danh-sach-cac-ngan-hang.chn"
-CHUNG_KHOAN_URL = "https://cafef.vn/du-lieu/danh-sach-cac-cong-ty-chung-khoan.chn"
 BASE_DIR = "/home/m1nhd3n/Works/DataEngineer/DataFlow2025Task2"
 
 
@@ -19,71 +21,79 @@ BASE_DIR = "/home/m1nhd3n/Works/DataEngineer/DataFlow2025Task2"
     start_date=datetime(2024, 3, 1)
 )
 def extract_data_from_sources():
-    @task(retries=3, retry_delay=timedelta(minutes=1))
-    def get_sub_companies_task(df: pd.DataFrame):
-        save_path = os.path.join(BASE_DIR, "data/raw/subcompanies.csv")
-        if os.path.exists(save_path):
-            return pd.read_csv(save_path, sep="|")
-        df = get_sub_companies(df)
-        df.to_csv(save_path, sep="|")
-        return df
+    start = EmptyOperator(task_id="start")
+    skip_create_db = EmptyOperator(task_id="skip_create_db")
+
+    load_industries_to_db = PythonOperator(
+        task_id="load_industries_into_db",
+        python_callable=insert_industries
+    )
+
+    check_condition_pg = BranchPythonOperator(
+        task_id="check_condition",
+        python_callable=branching_postgres
+    )
+
+    initiate_postgres = BashOperator(
+        task_id='initiate_postgres',
+        bash_command='PGPASSWORD="12" pg_restore -U postgres -h localhost -d '
+                     'vn_stock_dw /home/m1nhd3n/Works/DataEngineer/DataFlow2025Task2/scripts/sqls/initial.sql',
+        env={'PGPASSWORD': '12'},
+    )
 
     @task(retries=3, retry_delay=timedelta(minutes=1))
-    def get_vn30_list_task():
-        save_path = os.path.join(BASE_DIR, "data/raw/vn30_list.csv")
+    def get_list():
+        stock_codes = [
+            "ACB", "BCM", "BID", "BVH", "CTG", "FPT", "GAS", "GVR", "HDB", "HPG",
+            "LPB", "MBB", "MSN", "MWG", "PLX", "SAB", "SHB", "SSB",
+            "SSI", "STB",
+            "TCB", "TPB", "VCB", "VHM", "VIC", "VJC", "VNM", "VPB", "VRE"
+        ]
+        save_path = os.path.join(BASE_DIR, "data/raw/stock_codes_list.csv")
         if os.path.exists(save_path):
-            return pd.read_csv(save_path, sep="|")
-        df = get_vn30_list()
-        df.to_csv(save_path, sep='|')
-        return df
-
-    @task(retries=3, retry_delay=timedelta(minutes=1))
-    def get_industry_code_of_enterprises(df: pd.DataFrame):
-        save_path = os.path.join(BASE_DIR, 'data/raw/e_w_info_industry.csv')
-        if os.path.exists(save_path):
-            return pd.read_csv(save_path, sep="|")
-        df = get_all_enterprises_industry_code(df)
-        df.to_csv(save_path, sep="|")
-        return df
-
-    @task(retries=3, retry_delay=timedelta(minutes=1))
-    def get_more_enterprises_information(df: pd.DataFrame):
-        save_path = os.path.join(BASE_DIR, 'data/raw/e_w_info.csv')
-        if os.path.exists(save_path):
-            return pd.read_csv(save_path, sep='|')
-        df = get_all_enterprise_information(df)
-        df.to_csv(save_path, sep='|')
+            return pd.read_csv(save_path, sep="|", index_col=False)
+        df = pd.DataFrame({"stock_code": stock_codes})
+        df.to_csv(save_path, sep='|', index=False)
         return df
 
     @task(retries=3, retry_delay=timedelta(minutes=1))
     def get_industries_table():
         save_path = os.path.join(BASE_DIR, 'data/raw/industries.csv')
         if os.path.exists(save_path):
-            return pd.read_csv(save_path, sep='|')
+            return pd.read_csv(save_path, sep='|', index_col=False)
         df = get_industries_data()
-        df.to_csv(save_path, sep='|')
+        df.to_csv(save_path, sep='|', index=False)
         return df
 
-    @task()
-    def aggregate_companies(df_list=None):
+    @task(retries=3, retry_delay=timedelta(minutes=1))
+    def get_enterprises_data(df):
         try:
-            dfs = []
-            data_folder = os.path.join(BASE_DIR, 'data/raw/enterprises')
+            data_folder = os.path.join(BASE_DIR, 'data/raw')
             if os.path.exists(os.path.join(data_folder, 'enterprises.csv')):
-                return pd.read_csv(os.path.join(data_folder, 'enterprises.csv'))
-            if df_list is None:
-                filenames = os.listdir(data_folder)
-                for filename in filenames:
-                    dfs.append(pd.read_csv(os.path.join(data_folder, filename), sep="|"))
-            else:
-                dfs = df_list
+                df = pd.read_csv(os.path.join(data_folder, 'enterprises.csv'), sep="|", index_col=False)
+                df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+                return df
 
-            df = pd.concat(dfs, axis="rows", ignore_index=True)
             df = df.drop_duplicates(subset=['code'])
             df.loc[df['code'] == 'ABA', 'name'] = 'Công ty cổ phần giải pháp thương mại A BA'
             df.loc[df['code'] == 'CRV', 'name'] = 'Công ty Cổ phần Tập đoàn Bất động sản CRV'
             df.loc[df['code'] == 'VTT', 'name'] = 'Công ty Cổ Phần Công nghệ Việt Thành'
-            df.to_csv(os.path.join(data_folder, "enterprises.csv"))
+
+            print("Getting enterprises information")
+            df = get_all_enterprise_information(df)
+
+            print("Getting industry codes")
+            df = get_all_enterprises_industry_code(df)
+            df.loc[df['code'] == 'BID', 'industry_code'] = '6419'
+            df.loc[df['code'] == 'PDR', 'industry_code'] = '6810'
+
+            print("Getting sub companies")
+            sub_companies = get_sub_companies(df)
+
+            print("Saving DFs")
+            df.to_csv(os.path.join(data_folder, "enterprises.csv"), sep="|", index=False)
+            sub_companies.to_csv(os.path.join(data_folder, "sub_companies.csv"), sep="|", index=False)
+
             return df
         except Exception as e:
             print(f"Error: {e}")
@@ -93,68 +103,37 @@ def extract_data_from_sources():
         try:
             site = 'niem_yet'
             url = NIEM_YET_URL
-            data_folder = os.path.join(BASE_DIR, 'data/raw/enterprises')
+            data_folder = os.path.join(BASE_DIR, 'data/raw')
             save_path = os.path.join(data_folder, site + ".csv")
             if os.path.exists(save_path):
-                return pd.read_csv(save_path, sep='|')
+                return pd.read_csv(save_path, sep='|', index_col=False)
             comp_list = comp_list['stock_code'].tolist()
             data = process_url(url, comp_list)
-            data.to_csv(save_path, sep="|")
+            data.to_csv(save_path, sep="|", index=False)
             return data
         except Exception as e:
             print(f"Error: {e}")
 
-    @task(retries=3, retry_delay=timedelta(minutes=1))
-    def extract_banks(comp_list: pd.DataFrame):
-        try:
-            site = 'ngan_hang'
-            url = NGAN_HANG_URL
-            data_folder = os.path.join(BASE_DIR, 'data/raw/enterprises')
-            save_path = os.path.join(data_folder, site + ".csv")
-            if os.path.exists(save_path):
-                return pd.read_csv(save_path, sep='|')
-            comp_list = comp_list['stock_code'].tolist()
+    load_enterprises_into_db = PythonOperator(
+        task_id="load_enterprises_into_db",
+        python_callable=insert_enterprise
+    )
 
-            data = process_url(url, comp_list)
-            data.to_csv(save_path, sep="|")
-            return data
-        except Exception as e:
-            print(f"Error: {e}")
+    load_sub_companies_into_db = PythonOperator(
+        task_id="load_sub_companies_into_db",
+        python_callable=insert_sub_companies
+    )
 
-    @task(retries=3, retry_delay=timedelta(minutes=1))
-    def extract_stocks(comp_list: pd.DataFrame):
-        try:
-            site = 'chung_khoan'
-            url = CHUNG_KHOAN_URL
-            data_folder = os.path.join(BASE_DIR, 'data/raw/enterprises')
-            save_path = os.path.join(data_folder, site + ".csv")
-            if os.path.exists(save_path):
-                return pd.read_csv(save_path, sep='|')
-            comp_list = comp_list['stock_code'].tolist()
-            data = process_url(url, comp_list)
-            data.to_csv(save_path, sep="|")
-            return data
-        except Exception as e:
-            print(f"Error: {e}")
+    end = EmptyOperator(task_id="end")
 
-    @task(retries=3, retry_delay=timedelta(minutes=1))
-    def fix_missing_industry_code(df: pd.DataFrame):
-        save_path = os.path.join(BASE_DIR, 'data/raw/e_w_i_fix.csv')
-        if os.path.exists(save_path):
-            return pd.read_csv(save_path, sep="|")
-        df.loc[df['code'] == 'BID', 'industry_code'] = '6419'
-        df.loc[df['code'] == 'PDR', 'industry_code'] = '6810'
-        df.to_csv(save_path, sep="|")
-        return df
-
-    vn30 = get_vn30_list_task()
+    vn30 = get_list()
     niem_yet = extract_niem_yet(vn30)
     industry_table = get_industries_table()
-    aggregate_comps = aggregate_companies([niem_yet])
-    enterprises_w_info = get_more_enterprises_information(aggregate_comps)
-    enterprises_w_industry = get_industry_code_of_enterprises(enterprises_w_info)
-    e_w_i_fix = fix_missing_industry_code(enterprises_w_industry)
-    sub_companies = get_sub_companies_task(e_w_i_fix)
+    get_ent_data = get_enterprises_data(niem_yet)
+
+    start >> check_condition_pg >> [initiate_postgres, skip_create_db] >> end
+    start >> vn30 >> niem_yet >> get_ent_data >> load_enterprises_into_db >> load_sub_companies_into_db >> end
+    start >> industry_table >> load_industries_to_db >> load_enterprises_into_db
 
 
 dag = extract_data_from_sources()
